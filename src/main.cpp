1c67ec4f10e9798cbad15431d251048ee3d7f705
minumn <taishjort@gmail.com>
2019-11-28 16:12:08 +0100

    Trying to make Armadillo work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
-#include "MatrixAlgebra.h"
+#define ARMA_DONT_USE_WRAPPER
+#include <armadillo>
 
-int main(){
+#include "MatrixAlgebra.h"
+#include <iostream>
+
+using namespace std;
+using namespace arma;
+
+
+  
+void setup(){
     Matrix<3,3> matrix1;
     float data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     matrix1.fill(data);
+/*
+    mat A = randu<mat>(4,5);
+    mat B = randu<mat>(4,5);
+  
+    cout << A*B.t() << endl;
+    */
+}
 
-    return 0;
+void loop(){
+      
 }
