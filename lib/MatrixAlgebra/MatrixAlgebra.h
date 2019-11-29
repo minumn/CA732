c65a28e7c3c0ea107ebe2b9b692851cf1d009363
minumn <taishjort@gmail.com>
2019-11-29 13:44:22 +0100

    work in progress

--- lib/MatrixAlgebra/MatrixAlgebra.h
+++ lib/MatrixAlgebra/MatrixAlgebra.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <string>
 
 template <int rowSize, int colSize>
 class Matrix
@@ -8,18 +8,64 @@ private:
     int colSize_ = colSize;
     float content[rowSize][colSize];
 public:
-    Matrix();
-    ~Matrix();
+    Matrix(){};
+    ~Matrix(){};
     
     void fill(float data[rowSize * colSize]){
-        for(int i = 0; i > rowSize; ++i){
-            for(int j = 0; j > colSize; ++j){
-                //content[i][j] = data[j+i];
-                std::cout << data[i+j] << std::endl;;
+        content = data;
+    }
+    void fill(float data[rowSize][colSize]){
+        std::copy(&data[0][0], &data[0][0]+colSize*rowSize, &content[0][0]);
+    }
+
+    void fill(float data){
+        for(int i = 0; i < rowSize; ++i){
+            for(int j = 0; j < colSize; ++j){
+                content[i][j] = data;
             }
         }
     }
 
+    void print(){
+        Serial.println("#-----------------------#");
+        for(int i = 0; i < rowSize; ++i){
+            String str = String("| ");
+            for (size_t j = 0; j < colSize; ++j)
+            {            
+                str += content[i][j];
+                str += " | ";
+            }
+            Serial.println(str);
+        }
+        Serial.println("#-----------------------#");
+    }
 
+    float get(int row, int col){
+        return content[row][col];
+    }
+
+    void operator=(Matrix<rowSize, colSize> M2){
+        this->fill(M2.content);
+    }
+
+    template<int T>
+    Matrix<rowSize, T> operator*(Matrix<colSize, T> M2){
+        // rowsizeXcolsize * colsizeXT = rowsizeXT
+        float res[rowSize][T];
+
+        for (size_t i = 0; i < rowSize; ++i){
+            for (size_t j = 0; j < T; ++j){
+                res[i][j] = 0;
+
+                for (size_t k = 0; k < colSize; ++k){
+                    res[i][j] += this->content[i][k] + M2.get(k,j);
+                }   
+            }
+        }
+
+        Matrix<rowSize,T> mat;
+        mat.fill(res);
+        return mat;
+    }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
-#define ARMA_DONT_USE_WRAPPER
-#include <armadillo>
-
+#include <statusfunctions.h>
 #include "MatrixAlgebra.h"
-#include <iostream>
-
-using namespace std;
-using namespace arma;
 
 
-  
+Matrix<2,3> matrix;
+Matrix<3,2> matrix2;
+
+float data[9] = {10,2,3,4,5,6,7,8,9};
+float data2;
+
 void setup(){
-    Matrix<3,3> matrix1;
-    float data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    matrix1.fill(data);
-/*
-    mat A = randu<mat>(4,5);
-    mat B = randu<mat>(4,5);
-  
-    cout << A*B.t() << endl;
-    */
+    Serial. begin(250000);
 }
 
 void loop(){
-      
+    matrix.fill(12.0);
+    float a[2][3] = {1,1,1, 1,1,1};
+    float b[3][2] = {1,1,1, 1,1,1};
+ 
+    Serial.println("");
+    matrix.print();
+    delay(1000);
+
+    Serial.println("");
+    matrix.fill(a);
+    matrix2.fill(b);
+    auto matrix3 = matrix2*matrix;
+    matrix3.print();
+    delay(1000);
+
+
+
+
+    
 }
+
+
