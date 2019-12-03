2a355c28794ac014bd3048fe5673029c920c7728
brodersalza <eniels15@student.aau.dk>
2019-12-03 13:25:51 +0100

    Old main file, and matrix test cpp file

---
+++ src/MatrixLibTest.cpp
@@ -0,0 +1,84 @@
+/*
+#include "MatrixAlgebra.h"
+
+int main(){
+    Matrix<3,3> matrix1;
+    float data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    matrix1.fill(data);
+
+    return 0;
+}
+*/
+
+
+//#include <FlexCAN.h> // Pawelsky flex can library, used with Jens 
+//#include <SMC66Registers.h> // is a dictionary with some of the registers of the motor 
+//#include <Messenger.h>  // is a library for communication with arduino, not mine, but someone else 
+//#include <CANsmc.h> // My library for communication with the motors 
+//#include <math.h> // this is arduino standard math library
+//#include <Chrono.h> // a timing library, useful for timing control loops 
+#include <statusfunctions.h> // I believe an unuseful library, not finished. 
+#include <MatrixMath.h>
+
+#define N  (4)
+
+mtx_type A[N][N];
+mtx_type B[N][N];
+mtx_type C[N][N];
+mtx_type v[N];      // This is a row vector
+mtx_type w[N];
+mtx_type S[N][N];
+
+mtx_type maxVal = 10;  // maxValimum random matrix entry range
+
+
+void setup()
+{
+    Serial.begin(250000);
+    pinMode(13,OUTPUT);
+
+    // Initialize matrices
+	for (int i = 0; i < N; i++)
+	{
+		v[i] = i + 1;                  // vector of sequential numbers
+		for (int j = 0; j < N; j++)
+		{
+			A[i][j] = random(maxVal) - maxVal / 2.0f; // A is random
+			if (i == j)
+			{
+				B[i][j] = 1.0f;                  // B is identity
+			}
+			else
+			{
+				B[i][j] = 0.0f;
+			}
+		}
+	}
+
+    S[0][0] = 0;
+    S[0][1] = 0;
+    S[1][0] = 0;
+    S[1][1] = 1;
+
+}
+
+
+ void loop()
+ { 
+    delay(500);
+    digitalWrite(13,1);
+    for(int i = 0; i<100; i++)
+    {
+	//Matrix.Invert(*A, N);
+    //Matrix.Multiply(*A, *B, N, N, N, *C);
+    Matrix.Add(*A, *B, N, N, *C);
+	//Serial.println("\n A:");
+	//Matrix.Print(*A, N, N, "A");
+    //Matrix.Print(*B, N, N, "B");
+    //Matrix.Print(*C, N, N, "C");
+    //delay(1000);
+    }
+    digitalWrite(13,0);
+    //while(1);
+}
+
