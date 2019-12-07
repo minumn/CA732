ddff771e9e90cc0a879036288a38b42dd127db15
minumn <taishjort@gmail.com>
2019-12-07 12:51:14 +0100

    trying to time.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <arduino.h>
 #include <OneLimb.h>
+#include <Chrono.h>
 
 OneLimb oneLimb;
+Chrono chrono(Chrono::MICROS);
 
 void LED(bool on){
 	digitalWrite(13,on);
@@ -23,24 +25,31 @@ void loop()
 	
 	double AbsMotorPosition = -1200;
 	int Tau = 0;
-
+	unsigned long endTime = 0;
+	unsigned long startTime = 0;
 	// Wait for CHRONO timer
 	LED(OFF);
 	delay(2000);
 	LED(ON); // LED on during calculations.
 	// Restart CHRONO timer
 	
-	for (size_t i = 0; i < 100; ++i)
+	startTime = millis();
+	for (size_t i = 0; i < 1000; ++i)
 	{
-		Tau = oneLimb.getTorque(AbsMotorPosition);
+		Tau += oneLimb.getTorque(AbsMotorPosition);
 		AbsMotorPosition += 100;
 	}
 
+	endTime = millis();
 	LED(OFF);
 
-	Serial.print("Torque: ");
-	Serial.print(Tau);
+	
+	Serial.print("Time: ");
+	Serial.print(endTime/1000.0);
 	Serial.println();
+	Serial.println(startTime);
+	Serial.println(endTime);
+	Serial.println(Tau);
 
 	// Check CHRONO timer to see time spent?
 
